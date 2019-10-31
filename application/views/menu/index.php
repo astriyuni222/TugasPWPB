b52b3e9279c61f494708de6acef1e9c717b1db00
astriyuni222 <astriyuni222@gmail.com>
2019-10-31 08:22:28 +0700

    TugasPWPB

---
+++ application/views/menu/index.php
@@ -0,0 +1,210 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+        <div class="col-lg-6">
+
+            <?= form_error('menu', '<div class="alert
+                alert-danger" role="alert">', '</div>'); ?>
+
+            <?= $this->session->flashdata('message'); ?>
+
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newMenuModal">Add New Menu</a>
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Menu</th>
+                        <th scope="col">Action</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($menu as $m) : ?>
+                        <tr>
+                            <th scope="row"><?= $i; ?></th>
+                            <td><?= $m['menu']; ?></td>
+                            <td>
+                                <a href="#" data-toggle="modal" data-target="#modalEdit<?= $m['id']; ?>" class=" badge 
+                                badge-primary" data-popup="tooltip" data-placement="top" title="Edit Data">
+                                    <i class="fa fa-fw fa-pencil-alt"></i>Edit</a>
+                                <a href="<?= base_url('menu/delete/') ?><?= $m['id']; ?>" class="badge badge-danger tombol" id="delete" name="delete">
+                                    <i class="fas fa-fw fa-trash-alt fa-sm"></i>Delete</a>
+                            </td>
+
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+<!-- Modal -->
+<div class="modal fade" id="newMenuModal" tabindex="-1" role="dialog" aria-labelledby="newMenuModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="newMenuModalLabel">Add New Menu</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('menu'); ?>" method="post">
+                <div class="modal-body">
+                    <div class="form group">
+                        <input type="text" class="form-control" id="menu" name="menu" placeholder="Menu name">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Edit -->
+<?php foreach ($menu as $row) : ?>
+    <div class="modal fade" id="modalEdit<?= $row['id'] ?>">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="menu-label">Edit Menu</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="<?= base_url('menu/editMenu'); ?>" method="post">
+                    <input type="hidden" readonly value="<?= $row['id']; ?>" name="menu_id" class="form-control">
+
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="menu" name="menu" placeholder="Menu name" value="<?= $row['menu']; ?>">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa fa-fw fa-pencil-alt fa-sm"></i>Edit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+<?php endforeach; ?>
+
+ 
+6 conreoller/admin.php
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+    public function __construct()
+
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Dashboard';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function role()
+    {
+        $data['title'] = 'Role';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $data['role'] = $this->db->get('user_role')->result_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/role', $data);
+        $this->load->view('templates/footer');
+    }
+
+
+    public function roleAccess($role_id)
+    {
+        $data['title'] = 'Role Access';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $data['role'] = $this->db->get_where('user_role', ['id' => $role_id])->row_array();
+
+        $this->db->where('id !=', 1);
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/role-access', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function changeAccess()
+    {
+        $menu_id = $this->input->post('menuId');
+        $role_id = $this->input->post('roleId');
+
+        $data = [
+            'role_id' => $role_id,
+            'menu_id' => $menu_id
+
+        ];
+
+        $result = $this->db->get_where('user_access_menu', $data);
+
+        if ($result->num_rows() < 1) {
+            $this->db->insert('user_access_menu', $data);
+        } else {
+            $this->db->delete('user_access_menu', $data);
+        }
+        $this->session->set_flashdata('message', '<div class="alert
+            alert-success" role="alert">Access changed!</div>');
+    }
+    //edit Role
+    public function editRole()
+    {
+        $this->form_validation->set_rules('role', 'Role', 'required|trim');
+        if ($this->form_validation->run() == false) {
+            $this->session->set_flashdata('error', "Data Gagal di Edit");
+            redirect('admin/role');
+        } else {
+            $this->db->set('role', $this->input->post('role'));
+            $this->db->where('id', $this->input->post('id'));
+            $this->db->update('user_role');
+            $this->session->set_flashdata('message', "Data Berhasil di Edit");
+            redirect('admin/role');
+        }
+    }
+
+    public function delete($id)
+    {
+        $this->db->delete('user_role', ['id' => $id]);
+        $this->session->set_flashdata('message', "Data Berhasil di Hapus");
+        redirect('admin/role');
+    }
+}
